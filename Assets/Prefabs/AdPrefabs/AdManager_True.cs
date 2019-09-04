bf05c2940c3a4e3c1eb71536b1d4151b264b0443
Djuloh <j.tien@rubika-edu.com>
2019-09-04 20:24:26 +0200

    Le dummy des ads marche bien, je teste demain sur mon téléphone

---
+++ Assets/Prefabs/AdPrefabs/AdManager_True.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Advertisements;
+using GoogleMobileAds.Api;
+
+
+public class AdManager_True : MonoBehaviour
+{
+
+    private InterstitialAd ShortAd;
+    private RewardedAd LongAd;
+    public string adID;
+    private void Start()
+    {
+        LoadShortAd();
+    }
+
+    public void LoadShortAd()
+    {
+
+
+#if UNITY_ANDROID
+        adID = "ca-app-pub-7371546355195021/4675857438";
+
+#else
+        adID="unexpected_platform";
+
+#endif
+
+        this.ShortAd = new InterstitialAd(adID);
+        AdRequest request = new AdRequest.Builder().Build();
+        
+        this.ShortAd.LoadAd(request);
+    }
+
+    public void ShowShortAd()
+    {
+        if (this.ShortAd.IsLoaded())
+        {
+            this.ShortAd.Show();
+        }
+
+    }
+
+
+    public void LoadLongAd()
+    {
+#if UNITY_ANDROID
+        adID = "ca-app-pub-7371546355195021/2031536317";
+#else
+        adID="unexpected_platform";
+
+#endif
+
+        this.LongAd = new RewardedAd(adID);
+    }
+
+
+
+
+}
