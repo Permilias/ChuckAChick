8ed0d055dd2f33d4b86ad06081ffdef781cd3dac
Djuloh <j.tien@rubika-edu.com>
2019-09-22 12:17:55 +0200

    La mula mula, taga dans les poumons Faut qu'on fe-ta fe-ta pour cette mula mula

---
+++ Assets/Prefabs/AdPrefabs/PurchaseManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class PurchaseManager : IStoreListener {
+
+    private IStoreController storecontroller;
+    private IExtensionProvider extensionsprovider;
+
+    public PurchaseManager()
+    {
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct("nombre_variable", ProductType.Consumable, new IDs
+        {
+            {"nombre_variable_google", GooglePlay.Name }
+
+        });
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        this.storecontroller = controller;
+        this.extensionsprovider = extensions;
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("Fail de l'achat. Très très content, j'adore");
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        return PurchaseProcessingResult.Complete;
+        
+    }
+
+    public void OnPurchaseFailed(Product i, PurchaseFailureReason p)
+    {
+        
+    }
+
+ 
+}
